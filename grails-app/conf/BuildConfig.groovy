d06126df3c58a590a3e49c94a3d583a0bd9c1b7c
Miguel Rugerio <mikerugerio@gmail.com>
2012-04-17 12:44:55 -0500

    Se indico cambio de puerto de tomcat

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,6 +6,9 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+//CAMBIO DE PUERTO
+grails.server.port.http=8088
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
