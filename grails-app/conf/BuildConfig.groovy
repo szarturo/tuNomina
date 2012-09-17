ef1cbc829aa30fc6b2edd09607f8f621f8724479
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2012-09-16 20:11:00 -0500

    Actualizacion de los plugins:
    resources, activiti-spring-security y modernizr
    
    Signed-off-by: MIGUEL <miguel@miguel-Satellite-L635.(none)>

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,18 +42,18 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
-        runtime ":resources:1.1.6"
-
+        runtime ":resources:1.2.RC2"
+		
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
         build   ":tomcat:$grailsVersion"
-		compile ":activiti-spring-security:0.4.8"
+		compile ":activiti-spring-security:0.4.9"
 		 
 		compile ":jquery-ui:1.8.15"
-		compile ":modernizr:2.0.6"
+		compile ":modernizr:2.5.3"
 		compile ":uploadr:0.5.8"
     }
 }
