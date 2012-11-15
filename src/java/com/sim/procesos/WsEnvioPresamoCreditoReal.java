73237e10a79afd4bd0a9d22e9ebbe8fd098a2ad4
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2012-11-15 13:39:19 -0600

    Implementacion temporal del servicio de envio de solicitud a Credito Real
    Signed-off-by: MIGUEL <miguel@miguel-Satellite-L635.(none)>

--- src/java/com/sim/procesos/WsEnvioPresamoCreditoReal.java
+++ src/java/com/sim/procesos/WsEnvioPresamoCreditoReal.java
@@ -35,19 +35,20 @@ public class WsEnvioPresamoCreditoReal implements JavaDelegate{
 		}
 		
 		Solicitud solicitud = new Solicitud();
-		solicitud.setReferencia(referencia);
-		solicitud.setDistribuidor("9999");
-		solicitud.setSucursal("1111");
-		solicitud.setVendedor("");
-		solicitud.setDap("0");
-		solicitud.setPercepciones(10D);
-		solicitud.setDeducciones(1D);
-		solicitud.setSolicitud("2".getBytes());
-		solicitud.setPagare("1".getBytes());
-		solicitud.setIdentificacion("1".getBytes());
-		solicitud.setDocadA("1".getBytes());
-		solicitud.setDocadB("1".getBytes());
-		solicitud.setDocadC("1".getBytes());
+		solicitud.setReferencia(referencia); //Numero de Referencia o Folio propia del distribuidor o dejar en blanco
+		solicitud.setDistribuidor("9999"); //9999  Se asignara por CR
+		solicitud.setSucursal("1111"); //1111 Se asignara por CR
+		//EL VENDEDOR SE REFIEREN A MTN O EL VENDEDOR DE MTN
+		solicitud.setVendedor(""); //Clave de Vendedor asignada. Dejar en blanco para pruebas 
+		solicitud.setDap("0"); //Enviar un 0
+		solicitud.setPercepciones(10D); //Percepciones mensuales del cliente
+		solicitud.setDeducciones(1D); //Deducciones mensuales del cliente
+		solicitud.setSolicitud("2".getBytes()); //Imagen binarizada de la Solictud de credito
+		solicitud.setPagare("1".getBytes()); //Imagen binarizada del pagare
+		solicitud.setIdentificacion("1".getBytes()); //Imagen binarizada de la identificacion
+		solicitud.setDocadA("1".getBytes()); //Imagen binarizada de un documento adicional
+		solicitud.setDocadB("1".getBytes()); //Imagen binarizada de un documento adicional
+		solicitud.setDocadC("1".getBytes()); //Imagen binarizada de un documento adicional
 		
 		String consecutivo = null;
 		try {
@@ -57,6 +58,22 @@ public class WsEnvioPresamoCreditoReal implements JavaDelegate{
 			e.printStackTrace();
 		}
 		System.out.println("***CR*** Respuesta Credito Real Consecutivo: "+ consecutivo);
+		
+		//EN CASO DE EXISTIR MAS DE 6 DOCUMENTOS DEBEN SER ENVIADOS EN UN CICLO
+		//DEPENDIENDO DEL NUMERO DE DOCUMENTOS
+		
+		Adicional adicional = new Adicional();
+		adicional.setConsecutivo(consecutivo);
+		adicional.setDocAd("3".getBytes());
+		
+		boolean respuesta = false;
+		try {
+			respuesta = client.documentoAdicional(adicional);
+		} catch (ClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("**CRE** Respuesta envio documento adicional: "+ respuesta);		
 
 	}	
 
