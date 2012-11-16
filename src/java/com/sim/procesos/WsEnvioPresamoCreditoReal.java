643b9be39f6966946d74198cfc7aafa3a44253bc
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2012-11-15 18:08:59 -0600

    Se comento la funcionalidad Real para hacer pruebas desde GTS

--- src/java/com/sim/procesos/WsEnvioPresamoCreditoReal.java
+++ src/java/com/sim/procesos/WsEnvioPresamoCreditoReal.java
@@ -15,13 +15,13 @@ public class WsEnvioPresamoCreditoReal implements JavaDelegate{
 	//SERVICIO QUE ENVIA LA SOLICITUD DE UN PRESTAMO A CREDITO REAL
 	public void execute(DelegateExecution execution) {
 		
-		/*
+		
 		String comentarios = (String)execution.getVariable("comentarios");
 		comentarios = comentarios + " ,Envio de prestamo a Credito Real";
 		System.out.println("Credito Real Envio Credito. "+comentarios);
 		execution.setVariable("comentarios", comentarios);
-		*/
 		
+		/* CODIGO QUE ENVIA SOLICITUD Y DOCUMENTOS A CREDITO REAL
 		String referencia = (String)execution.getVariable("folioSolicitud");
 		System.out.println("REFERENCIA Envio CR: "+referencia);
 		
@@ -74,7 +74,7 @@ public class WsEnvioPresamoCreditoReal implements JavaDelegate{
 			e.printStackTrace();
 		}
 		System.out.println("**CRE** Respuesta envio documento adicional: "+ respuesta);		
-
+		*/
 	}	
 
 }
