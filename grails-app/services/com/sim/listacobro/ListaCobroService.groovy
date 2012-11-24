a82d4e7b06aac06f9ea94ae7f05b1b53f56d2619
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2012-11-24 12:02:32 -0600

    Se creo servicio Lista de Cobro para ejecutarlo desde el Bootstrap
    Signed-off-by: MIGUEL <miguel@miguel-Satellite-L635.(none)>

---
+++ grails-app/services/com/sim/listacobro/ListaCobroService.groovy
@@ -0,0 +1,24 @@
+package com.sim.listacobro
+
+import com.sim.entidad.EntDependencia
+
+class ListaCobroService {
+
+    //METODO UTILIZADO EN EL BOOTSTRAP
+    def generarListasCobro(EntDependencia dependencia,Integer anio){
+
+        Integer pagoNomina = 1
+
+        (1..dependencia.periodicidadPagoNomina.cantidadPagos).each{
+
+            new ListaCobro(anio:  anio,
+                    numeroPago: pagoNomina,
+                    parcialidades: false,
+                    dependencia: dependencia,
+            ).save(flush: true)
+
+            pagoNomina ++
+        }
+
+    }
+}
