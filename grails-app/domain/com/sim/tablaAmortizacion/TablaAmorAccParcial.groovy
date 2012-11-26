dcaa78cc265374a2b07a1132620443c3d0cbd95f
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2012-11-26 10:55:29 -0600

    Se definiron los atributos del Dominio TablaAmortizacion Accesorio Parcial
    Signed-off-by: MIGUEL <miguel@miguel-Satellite-L635.(none)>

--- grails-app/domain/com/sim/tablaAmortizacion/TablaAmorAccParcial.groovy
+++ grails-app/domain/com/sim/tablaAmortizacion/TablaAmorAccParcial.groovy
@@ -2,6 +2,20 @@ package com.sim.tablaAmortizacion
 
 class TablaAmorAccParcial {
 
+	BigDecimal  	importeAccesorioPagado
+	BigDecimal  	importeIvaAccesorioPagado
+
+	static belongsTo = [tablaAmortizacionAccesorio: TablaAmortizacionAccesorio,
+						tablaAmorRegParcial: TablaAmorRegParcial]
+
     static constraints = {
+    	importeAccesorioPagado()
+    	importeIvaAccesorioPagado()
+    	tablaAmorRegParcial()
+    	tablaAmortizacionAccesorio()
     }
+
+	String toString() {
+		"${tablaAmorRegParcial} - ${tablaAmortizacionAccesorio}"
+	}    
 }
