fd42faf36df427d2d77cc6c6cf31dee8c92e1bd8
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2012-11-27 09:58:52 -0600

    Ejemplo temporal de mostrar las listas de Cobro para una Dependencia
    Signed-off-by: MIGUEL <miguel@miguel-Satellite-L635.(none)>

--- grails-app/controllers/com/sim/entidad/EntDependenciaController.groovy
+++ grails-app/controllers/com/sim/entidad/EntDependenciaController.groovy
@@ -7,24 +7,23 @@ class EntDependenciaController {
     def scaffold = true
 
     def generarListasCobro = {
-
     	EntDependencia dependencia = EntDependencia.findByClaveDependencia(params.claveDependencia)
     	Integer anio = params.anio.toInteger()
-
-
     	Integer pagoNomina = 1
 
     	(1..dependencia.periodicidadPagoNomina.cantidadPagos).each{
-
 			new ListaCobro(anio:  anio,
 				numeroPago: pagoNomina,
 				parcialidades: false,
 				dependencia: dependencia,
 				).save(flush: true)
-
     		pagoNomina ++
     	}
-
-
     }
+
+    def showListaCobro = {
+        log.info("showListaCobro Inicio")
+        EntDependencia dependencia = EntDependencia.findByClaveDependencia(params.claveDependencia)
+    }
+
 }
