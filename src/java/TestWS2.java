6018e08b8b7f32b195c1d0a810f50fc0b4171aea
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-05 19:49:36 -0600

    Se probo el metodo SolicitudesDecididasDia

--- src/java/TestWS2.java
+++ src/java/TestWS2.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 import mx.com.creditoreal.ws.client.Client;
+import mx.com.creditoreal.ws.dto.SolicitudDecididasDia;
 import mx.com.creditoreal.ws.dto.SolicitudDia;
 
 
@@ -14,15 +15,15 @@ public class TestWS2 {
 		
 		Client cliente = new Client(true);
 		
-		List<SolicitudDia> solicitudes=cliente.getSolicitudesDia("9999", "20121213", "", "");
-		for(SolicitudDia s:solicitudes){
-			System.out.println(s.getNombre());
-		}
-		
-//		List<SolicitudDecididasDia> solicitudes2=cliente.getSolicitudesDecididasDia("9999", "20121213", "", "");
-//		for(SolicitudDecididasDia s : solicitudes2){
+//		List<SolicitudDia> solicitudes=cliente.getSolicitudesDia("9999", "20121213", "", "");
+//		for(SolicitudDia s:solicitudes){
 //			System.out.println(s.getNombre());
 //		}
+		
+		List<SolicitudDecididasDia> solicitudes2=cliente.getSolicitudesDecididasDia("9999", "20121213", "", "");
+		for(SolicitudDecididasDia s : solicitudes2){
+			System.out.println(s.getNombre());
+		}
 //		
 //		List<ComprasDia> compras=cliente.getComprasDia("9999", "20121213", "", "");
 //		for(ComprasDia c: compras){
