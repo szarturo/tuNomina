1c39de657b5faec3c67bac93cdb13e43c738a4a7
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-07 12:30:42 -0600

    Se asigno el rango de fechas para una lista de cobro

--- grails-app/services/com/sim/listacobro/ListaCobroService.groovy
+++ grails-app/services/com/sim/listacobro/ListaCobroService.groovy
@@ -6,22 +6,45 @@ import com.sim.catalogo.SimCatPeriodicidad
 class ListaCobroService {
 
     //METODO UTILIZADO EN EL BOOTSTRAP
-
     def generarListasCobro(EntDependencia dependencia,
             Integer anio,
             SimCatPeriodicidad periodicidad){
 
-        Integer pagoNomina = 1
+        Integer numeroPago = 1
+        Integer mes = 1
+        Integer mesSegundaQuincena = 0
+        Date    fechaSegundaQuincena
 
         (1..periodicidad.cantidadPagos).each{
 
-            new ListaCobro(anio:  anio,
-                    numeroPago: pagoNomina,
+            ListaCobro listaCobro = new ListaCobro(anio:  anio,
+                    numeroPago: numeroPago,
                     parcialidades: false,
                     dependencia: dependencia,
-            ).save()
+            )
 
-            pagoNomina ++
+            //SE OBTIENE SI VA A CORRESPONDER A UNA QUINCENA DE INICIO DEL MES
+            Integer resultadoPar = numeroPago.mod(2)
+            if (resultadoPar==1){
+                //CORRESPONDE A LA PRIMERA QUINCENA DEL MES
+                listaCobro.fechaInicio = new Date("${mes}/01/${anio}")
+                listaCobro.fechaFin = listaCobro.fechaInicio + 14
+                mes++
+                //SE GUARDA LA FECHA DE INICIO PARA LA SEGUNDA QUINCENA
+                fechaSegundaQuincena = listaCobro.fechaFin + 1
+            }else{
+                //CORRESPONDE A LA SEGUNDA QUINCENA DEL MES
+                listaCobro.fechaInicio = fechaSegundaQuincena
+                //SE RECUPERA EL ULTIMO DIA DEL MES
+                //Note: Months are from 0 to 11
+                Calendar calendar = GregorianCalendar.instance
+                calendar.set(anio, mesSegundaQuincena, 20) //20 NO AFECTA CALCULO
+                Integer ultimoDiaMes = calendar.getActualMaximum(GregorianCalendar.DAY_OF_MONTH)
+                listaCobro.fechaFin = new Date("${mesSegundaQuincena+1}/${ultimoDiaMes}/${anio}")
+                mesSegundaQuincena ++
+            }
+            numeroPago ++
+            listaCobro.save()
         }
 
     }
