ad579b7c413685e5c39688f3505260fc18f91188
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-09 20:21:45 -0600

    Se elimino metodo showListaCobro que no tiene funcionalidad

--- grails-app/controllers/com/sim/entidad/EntDependenciaController.groovy
+++ grails-app/controllers/com/sim/entidad/EntDependenciaController.groovy
@@ -7,11 +7,6 @@ class EntDependenciaController {
 
     def scaffold = true
 
-    def showListaCobro = {
-        log.info("showListaCobro Inicio")
-        EntDependencia dependencia = EntDependencia.findByClaveDependencia(params.claveDependencia)
-    }
-
     def ajaxGetTipoEmpleado = {
 		EntDependencia dependencia = EntDependencia.get(params.id)
     	render dependencia?.tiposEmpleado as JSON
