752e9289592582e47ea352d552f2193f2e3042d9
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-21 14:47:45 -0600

    Se crea en el servicio el registro PublicacionLote

--- grails-app/services/com/sim/publicacion/PublicacionLoteService.groovy
+++ grails-app/services/com/sim/publicacion/PublicacionLoteService.groovy
@@ -1,8 +1,65 @@
 package com.sim.publicacion
 
+import com.sim.usuario.Usuario
+import com.sim.listacobro.ListaCobroDetalle
+import com.sim.listacobro.ListaCobroDetalleEstatus
+import com.sim.pfin.PfinCatParametro
+
+class PublicacionLoteException extends RuntimeException {
+	String mensaje
+}
+
 class PublicacionLoteService {
 
+	static transactional = true
+    //SERVICIO PARA RECUPERAR EL USUARIO
+    def springSecurityService    	
+
     def publicar() {
-    	log.info ("Publicar")
+    	Usuario usuario = springSecurityService.getCurrentUser()
+    	log.info ("Usuario: ${usuario}")
+
+    	//SE OBTIENE LA FECHA DEL MEDIO
+		PfinCatParametro parametros = PfinCatParametro.findByClaveMedio("SistemaMtn")
+		Date fechaMedio = parametros?.fechaMedio
+		if (!fechaMedio){
+			throw new PublicacionLoteException(mensaje: "No existe la fecha del medio")
+		}
+
+    	//SE CREA EL REGISTRO DE LA PUBLICACION LOTE
+		PublicacionLote publicacionLote =
+				new PublicacionLote(fechaMedio:  fechaMedio,
+					fechaRegistro: new Date(),
+					importeLote: 0,
+					usuario: usuario,
+					).save()
+
+
+		def criteriaDetallesListaCobro = 
+			ListaCobroDetalle.createCriteria()
+		ArrayList detalleRegistros = 
+			criteriaDetallesListaCobro.list() {
+			and {
+				eq("usuario",usuario)
+				le("estatus",ListaCobroDetalleEstatus.APLICADO)
+			}
+		}
+
+		Double totalPublicacion = 0
+
+		detalleRegistros.each{
+			log.info ("Detalle: ${it}")
+			totalPublicacion = totalPublicacion + it.pago.importePago
+			//SE CREA EL DETALLE DE LA PUBLICACION
+			/*
+			new SimCatPeriodicidad(clavePeriodicidad:  'SEMANA',
+					nombrePeriodicidad: 'SEMANAL',
+					cantidadPagos: 52,
+					numeroDias: 7,
+					).save(failOnError: true)
+			*/
+
+
+		}
     }
 }
