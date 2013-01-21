37231897ae930a647a744704081bd6bd6397e7ea
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-21 17:37:41 -0600

    Se definio la creacion del detalle de la publicacion

--- grails-app/services/com/sim/publicacion/PublicacionLoteService.groovy
+++ grails-app/services/com/sim/publicacion/PublicacionLoteService.groovy
@@ -34,7 +34,6 @@ class PublicacionLoteService {
 					usuario: usuario,
 					).save()
 
-
 		def criteriaDetallesListaCobro = 
 			ListaCobroDetalle.createCriteria()
 		ArrayList detalleRegistros = 
@@ -47,18 +46,74 @@ class PublicacionLoteService {
 
 		Double totalPublicacion = 0
 
+		String usuarioWs = 'Usuario'
+		String passwordWs = 'Password'
+		//IMPLEMENTAR QUE SEA INCREMENTABLE E IRREPETIBLE
+		Integer idWs = 1
+		//NUMERO DE CLIENTE QUE SE ASIGNO AL CREDITO POR PARTE DE CR
+		String numeroClienteWs
+		//NUMERO DE OPERACION QUE SE ASIGNO AL CREDITO POR PARTE DE CR
+		String numeroOperacionWs		
+		String claveCiaWs = '999'
+		//SE REFIERE A LA RESPUESTA DE CR: ClaveTienda	
+		String claveSucursalWs = '001'
+		//EN QUE CASOS SE ENVIA 'CAN' PARA CANCELACIONES
+		String tipoPagoWs = 'NOR'		
+		String conceptoWs = '51'
+		BigDecimal importePagoWs 
+		BigDecimal importeMoratoriosWs = 0
+		//SE REFIERE A LA RESPUESTA DE CR: Referencia
+		String referenciaWs		
+		String fechaPagoMesWs	
+		String fechaPagoDiaWs	
+		String fechaPagoAnioWs			
+
 		detalleRegistros.each{
 			log.info ("Detalle: ${it}")
+			//SE INDICA EL IMPORTE TOTAL DE LA PUBLICACION
 			totalPublicacion = totalPublicacion + it.pago.importePago
-			//SE CREA EL DETALLE DE LA PUBLICACION
-			/*
-			new SimCatPeriodicidad(clavePeriodicidad:  'SEMANA',
-					nombrePeriodicidad: 'SEMANAL',
-					cantidadPagos: 52,
-					numeroDias: 7,
-					).save(failOnError: true)
-			*/
 
+			//SE OBTIENE EL NUMERO DE CLIENTE
+			//FALTA DAR FUNCIONALIDAD AL DOMINIO DatosCrRespuesta
+			numeroClienteWs = '11' //it.pago.prestamo.datosCrRespuesta.numeroCliente
+
+			//SE OBTIENE EL NUMERO DE OPERACION
+			//FALTA DAR FUNCIONALIDAD AL DOMINIO DatosCrRespuesta
+			numeroOperacionWs = '22' //it.pago.prestamo.datosCrRespuesta.numeroOperacion
+
+			importePagoWs = it.pago.importePago
+
+			referenciaWs = '0000099990-1' // it.pago.prestamo.datosCrRespuesta.referencia
+
+			Date fechaPago = it.pago.fechaPago
+			Calendar calFechaPago = fechaPago.toCalendar()
+
+			fechaPagoAnioWs = calFechaPago.get(Calendar.YEAR)
+			fechaPagoMesWs = calFechaPago.get(Calendar.MONTH) + 1
+			fechaPagoDiaWs = calFechaPago.get(Calendar.DATE)
+
+			//SE CREA EL DETALLE DE LA PUBLICACION
+			PublicacionDet publicacionDet = new PublicacionDet(
+				usuario:  usuarioWs,
+				password: passwordWs,
+				idCr: idWs,
+				numeroCliente: numeroClienteWs,
+				numeroOperacion: numeroOperacionWs,
+				claveCia: claveCiaWs,
+				claveSucursal: claveSucursalWs,
+				tipoPago: tipoPagoWs,
+				concepto:conceptoWs,
+				importePago: importePagoWs,
+				importeMoratorios: importeMoratoriosWs,
+				referencia: referenciaWs,
+				fechaPagoMes: fechaPagoMesWs,
+				fechaPagoDia: fechaPagoDiaWs,
+				fechaPagoAnio: fechaPagoAnioWs,
+				publicacionLote: publicacionLote,
+				listaCobroDetalle: it,				
+				).save(failOnError: true)
+
+			log.info ("OKKKK")
 
 		}
     }
