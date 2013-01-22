f146f3513fa328b7343b560e2a0c4b3052a99fd1
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-21 18:59:07 -0600

    Se definieron los atributos para los dominios de las respuestas de Credito Real
    Signed-off-by: MIGUEL <miguel@miguel-Satellite-L635.(none)>

--- grails-app/domain/com/sim/credito/PrestamoCrComprada.groovy
+++ grails-app/domain/com/sim/credito/PrestamoCrComprada.groovy
@@ -2,9 +2,56 @@ package com.sim.credito
 
 class PrestamoCrComprada {
 
+	//DOMINIO QUE ALMACENA LA INFORMACION DE LOS CREDITOS COMPRADOS
+
+	String numeroSolicitud
+	String numeroOperacion
+	String claveCia
+	String claveSucursal
+	String nombre
+	String fechaCompra
+	String tipoPromocion
+	String clasificador
+	String primerCredito
+	String status
+	BigDecimal importeCedido
+	BigDecimal ivaCapital
+	BigDecimal ivaDiferido
+	BigDecimal ivaIntereses
+	BigDecimal importeDescuento
+	BigDecimal pagoTienda
+	BigDecimal reserva
+	BigDecimal netoPagado
+	BigDecimal importe 
+	Integer cesion
 	Prestamo prestamo
 
     static constraints = {
-    	unique: true
+		numeroSolicitud()
+		numeroOperacion()
+		claveCia()
+		claveSucursal()
+		nombre()
+		fechaCompra()
+		tipoPromocion()
+		clasificador()
+		primerCredito()
+		status()
+		importeCedido()
+		ivaCapital()
+		ivaDiferido()
+		ivaIntereses()
+		importeDescuento()
+		pagoTienda()
+		reserva()
+		netoPagado()
+		importe()
+		cesion()
+		prestamo()
     }
+
+	String toString() {
+		"${prestamo} - Importe Cedido: ${importeCedido}"
+	}    
+
 }
--- grails-app/domain/com/sim/credito/PrestamoCrRespuesta.groovy
+++ grails-app/domain/com/sim/credito/PrestamoCrRespuesta.groovy
@@ -2,9 +2,50 @@ package com.sim.credito
 
 class PrestamoCrRespuesta {
 
+	//DOMINIO QUE ALMACENA LAS RESPUESTAS DE CREDITO REAL
+	//ES DECIR CON ESTATUS DE ACTIVO
+
+	String consecutivo	
+	Integer folio	
+	String referencia	
+	String claveDistribuidor	
+	String claveTienda	
+	String nombreSucursal	
+	String fechaRecepcion
+	String fechaRespuesta
+	BigDecimal montoSolicitado
+	BigDecimal montoAutorizado
+	String estatus	
+	String motivo	
+	String nombre	
+	String vendedor	
+	String promocion	
+	String observaciones	
+	String numeroCliente	
 	Prestamo prestamo
 
     static constraints = {
-    	unique: true
+		 consecutivo()
+		 folio()	
+		 referencia()
+		 claveDistribuidor()
+		 claveTienda()
+		 nombreSucursal()
+		 fechaRecepcion()
+		 fechaRespuesta()
+		 montoSolicitado()
+		 montoAutorizado()
+		 estatus()	
+		 motivo()	
+		 nombre()	
+		 vendedor()
+		 promocion()	
+		 observaciones()
+		 numeroCliente()	
+		 prestamo()    	
     }
+
+	String toString() {
+		"${prestamo} - ${referencia}"
+	}    
 }
