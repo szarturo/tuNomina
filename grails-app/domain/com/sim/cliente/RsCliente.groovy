500df11dbe8392582aab927bfad11d6b07bedbb8
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-01-28 17:20:38 -0600

    Se eliminaron lineas comentadas

--- grails-app/domain/com/sim/cliente/RsCliente.groovy
+++ grails-app/domain/com/sim/cliente/RsCliente.groovy
@@ -11,19 +11,6 @@ class RsCliente {
     EntDependencia dependencia
     String         numeroDeNomina
 
-    /*
-    BigDecimal ingresoSemanal
-    Integer    dependientesEconomicos
-    String     destinoDelCredito
-    String     rolEnElHogar
-    Boolean    listaNegra = false
-    */
-
-    /*
-    static hasMany = [ cuentasBancarias : RsCuentaBancaria, documentacion : RsClienteDocumentacion, referenciasClientes : RsReferenciaCliente,
-            negocios : SimClienteNegocio, integranteUef : SimClienteIntegranteUef, adeudos : SimClienteAdeudos, garantias : SimClienteGarantia ]
-     */
-
     static hasMany = [ creditos : Prestamo, 
                        cuentas : PfinCuenta, 
                        cuentaBancaria : RsClienteCtaBancaria,
@@ -39,32 +26,6 @@ class RsCliente {
         cuentaBancaria nullable: true
         clienteEmpleo nullable: true
         referenciasClientes nullable : true
-
-        /*
-        ingresoSemanal scale:2, nullable:true
-        dependientesEconomicos range:0..30
-        destinoDelCredito inList:[
-                "ADQUIRIR O COMPRAR MERCANCIA",
-                "COMPRAR MAQUINARIA, EQUIPO O HERRAMIENTAS",
-                "AMPLIAR, ADECUAR O REPARAR EL LOCAL O VEHICULO",
-                "COMPRAR LOCAL O VEHICULO",
-                "PAGAR DEUDAS DEL NEGOCIO",
-                "OTRO FIN RELACIONADO",
-                "FINES AJENOS AL NEGOCIO"]
-        rolEnElHogar inList:[
-                "JEFE(A)",
-                "ESPOSO(A)",
-                "HIJO(A)",
-                "OTRO"]
-        listaNegra()
-        */
-        //cuentasBancarias nullable : true
-        //documentacion nullable : true
-        //referenciasClientes nullable : true
-        //negocios ()
-        //integranteUef nullable : true
-        //adeudos nullable : true
-        //garantias nullable : true
     }
 
     String toString() {
