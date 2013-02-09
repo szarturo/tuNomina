1c93c7def0f13027212c8fb33ffb2a04f2b440ff
MIGUEL <miguel@miguel-Satellite-L635.(none)>
2013-02-09 09:37:10 -0600

    Datos iniciales de Sepomex

---
+++ scripts/sepomex/datosTablas/ENT_REGION.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  File created - Friday-February-08-2013   
+--------------------------------------------------------
+REM INSERTING into ENT_REGION
+Insert into ENT_REGION (ID,VERSION,CLAVE_REGION,DESCRIPCION_REGION,NOMBRE_REGION) values (1,0,'REGIÓN GENERAL','REGIÓN GENERAL','REGIÓN GENERAL');
